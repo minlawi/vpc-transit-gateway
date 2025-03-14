8ae16cd5f8faef2e6cf4f95de35fd69a75f75fb3
minlawi <minlawi1992@gmail.com>
2025-03-15 00:20:16 +0800

    Updated networking.tf

--- networking.tf
+++ networking.tf
@@ -65,7 +65,7 @@ resource "aws_eip" "nat_eip" {
 resource "aws_nat_gateway" "nat_gateway" {
   count         = var.create_vpc ? 1 : 0
   allocation_id = aws_eip.nat_eip[0].id
-  subnet_id     = aws_subnet.public_subnets[0].id
+  subnet_id     = aws_subnet.public_subnets[1].id
   depends_on    = [aws_internet_gateway.igw]
   tags = {
     Name = "nat-gateway-vpc-1"
