e3ea0ba4508780493cabb199a3949d4bf3996c14
minlawi <minlawi1992@gmail.com>
2025-03-20 23:42:36 +0800

    Updated vpc_public.tf

--- vpc_public.tf
+++ vpc_public.tf
@@ -103,7 +103,7 @@ resource "aws_eip" "eip_nat" {
 resource "aws_nat_gateway" "nat_gateway" {
   count         = var.create_vpc ? 1 : 0
   allocation_id = aws_eip.eip_nat[0].id
-  subnet_id     = aws_subnet.subnet_nat[count.index].id
+  subnet_id     = aws_subnet.subnet_public[count.index].id
   depends_on    = [aws_internet_gateway.internet_gateway]
   tags = {
     Name = "nat-gateway"
