d69c0d005aeb6e76bd5759bdc19a58da18dc3ba2
minlawi <minlawi1992@gmail.com>
2025-03-20 23:59:15 +0800

    Added sg_private.tf

---
+++ sg_private.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "vpc_private_sg" {
+  count  = var.create_vpc ? 1 : 0
+  vpc_id = aws_vpc.vpc_private[0].id
+  name   = "vpc-private-sg"
+  tags = {
+    Name = "vpc-private-sg"
+  }
+}
+
+resource "aws_security_group_rule" "vpc_private_allow_ssh_ingress" {
+  count             = var.create_vpc ? 1 : 0
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vpc_private_sg[0].id
+  cidr_blocks       = [aws_vpc.vpc_public[0].cidr_block]
+  description       = "Allow SSH from public_vpc CIDR blocks"
+}
+
+resource "aws_security_group_rule" "vpc_private_allow_http_ingress" {
+  count             = var.create_vpc ? 1 : 0
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vpc_private_sg[0].id
+  cidr_blocks       = [aws_vpc.vpc_public[0].cidr_block]
+  description       = "Allow HTTP from public_vpc CIDR blocks"
+}
+
+resource "aws_security_group_rule" "vpc_private_allow_all_egress" {
+  count             = var.create_vpc ? 1 : 0
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.vpc_private_sg[0].id
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow all egress traffic"
+}
