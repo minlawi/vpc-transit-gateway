872181a310afd5ecc5053ab9b28a1c3a101d69ff
minlawi <minlawi1992@gmail.com>
2025-03-22 10:12:29 +0800

    Updated bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "vpc_public_bastion" {
   count           = var.create_vpc ? 1 : 0
   ami             = data.aws_ami.ubuntu.id
-  instance_type   = "t2.micro"
+  instance_type   = local.t2_micro
   key_name        = "my-key-pair"
   subnet_id       = aws_subnet.subnet_public[0].id
   security_groups = [aws_security_group.vpc_public_sg[0].id]
