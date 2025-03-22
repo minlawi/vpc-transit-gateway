2dac4831ddca57975738a80e2db493f5935c3265
minlawi <minlawi1992@gmail.com>
2025-03-22 09:51:41 +0800

    Added bastion.tf

---
+++ bastion.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "vpc_public_bastion" {
+  count           = var.create_vpc ? 1 : 0
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  key_name        = "my-key-pair"
+  subnet_id       = aws_subnet.subnet_public[0].id
+  security_groups = [aws_security_group.vpc_public_sg[0].id]
+  tags = {
+    Name = "bastion-host"
+  }
+  lifecycle {
+    ignore_changes = [security_groups]
+  }
+
+}
