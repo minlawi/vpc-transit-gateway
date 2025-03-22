28265c2f7461b1055b1343f00fffaadf8ca3daae
minlawi <minlawi1992@gmail.com>
2025-03-22 10:11:21 +0800

    Updated sg_alb.tf

--- sg_alb.tf
+++ sg_alb.tf
@@ -9,22 +9,22 @@ resource "aws_security_group" "vpc_private_alb_sg" {
 
 resource "aws_security_group_rule" "vpc_private_alb_sg_allow_http_ingress_private_subnets" {
   count             = var.create_vpc ? 1 : 0
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
+  type              = local.inbound
+  from_port         = local.http_port
+  to_port           = local.http_port
+  protocol          = local.http_protocol
   security_group_id = aws_security_group.vpc_private_alb_sg[0].id
   cidr_blocks       = [for subnet in aws_subnet.subnet_private : subnet.cidr_block]
   description       = "Allow HTTP traffic from Private Subnets"
 
 }
 
-resource "aws_security_group_rule" "allow_http_from_vpc_public" {
+resource "aws_security_group_rule" "vpc_private_alb_sg_allow_http_from_vpc_public" {
   count             = var.create_vpc ? 1 : 0
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
+  type              = local.inbound
+  from_port         = local.http_port
+  to_port           = local.http_port
+  protocol          = local.http_protocol
   cidr_blocks       = [aws_vpc.vpc_public[0].cidr_block]
   security_group_id = aws_security_group.vpc_private_alb_sg[0].id
   description       = "Allow HTTP traffic from VPC Public"
@@ -33,11 +33,11 @@ resource "aws_security_group_rule" "allow_http_from_vpc_public" {
 
 resource "aws_security_group_rule" "vpc_private_alb_sg_allow_all_egress" {
   count             = var.create_vpc ? 1 : 0
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
+  type              = local.outbound
+  from_port         = local.all_ports
+  to_port           = local.all_ports
+  protocol          = local.all_protocols
   security_group_id = aws_security_group.vpc_private_alb_sg[0].id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = [local.all_cidr_blocks]
   description       = "Allow all egress traffic"
 }
